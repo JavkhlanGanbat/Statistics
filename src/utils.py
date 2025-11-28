abf4f4f70fee88a4a453e579a6d22e50e78300dc
Javkhlan Ganbat <javkhlanganbat2003@gmail.com>
2025-11-28 22:31:46 +0800

    .

--- src/utils.py
+++ src/utils.py
@@ -4,10 +4,8 @@ import joblib
 MODEL_DIR = "../models"
 
 def save_model(model, filename="logreg_model.pkl"):
-    """Save model to disk."""
     os.makedirs(MODEL_DIR, exist_ok=True)
     joblib.dump(model, os.path.join(MODEL_DIR, filename))
 
 def load_model(filename="logreg_model.pkl"):
-    """Load model from disk."""
     return joblib.load(os.path.join(MODEL_DIR, filename))
